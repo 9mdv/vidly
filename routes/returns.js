0c79e775a3ead70782c2488ed459533538fd9dd4
g11v8a <g11v8a@gmail.com>
2018-10-14 19:34:53 +0800

    finished app :rocket:

--- routes/returns.js
+++ routes/returns.js
@@ -1,25 +1,38 @@
+const Joi = require('joi')
 const { Rental } = require('../models/rental')
+const { Movie } = require('../models/movie')
 const auth = require('../middleware/auth')
+const validate = require('../middleware/validate')
 const express = require('express')
 const router = express.Router()
 
-router.post('/', auth, async (req, res) => {
-  if (!req.body.customerId)
-    return res.status(400).send('customerId not provided')
-  if (!req.body.movieId) return res.status(400).send('movieId not provided')
+router.post('/', [auth, validate(validateReturn)], async (req, res) => {
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId)
 
-  const rental = await Rental.findOne({
-    'customer._id': req.body.customerId,
-    'movie._id': req.body.movieId
-  })
   if (!rental) return res.status(404).send('Rental not found')
   if (rental.dateReturned)
     return res.status(400).send('Return already processed')
 
-  rental.dateReturned = new Date()
+  rental.return()
   await rental.save()
 
-  return res.status(200).send()
+  await Movie.update(
+    { _id: rental.movie._id },
+    {
+      $inc: { numberInStock: 1 }
+    }
+  )
+
+  return res.send(rental)
 })
+
+function validateReturn(req) {
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required()
+  }
+
+  return Joi.validate(req, schema)
+}
 
 module.exports = router
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,6 +1,8 @@
+const moment = require('moment')
 const request = require('supertest')
 const mongoose = require('mongoose')
 const { Rental } = require('../../models/rental')
+const { Movie } = require('../../models/movie')
 const { User } = require('../../models/user')
 
 describe('/api/returns', () => {
@@ -8,6 +10,7 @@ describe('/api/returns', () => {
   let customerId
   let movieId
   let rental
+  let movie
   let token
 
   const exec = () => {
@@ -23,6 +26,15 @@ describe('/api/returns', () => {
     customerId = mongoose.Types.ObjectId()
     movieId = mongoose.Types.ObjectId()
     token = new User().generateAuthToken()
+
+    movie = new Movie({
+      _id: movieId,
+      title: '12345',
+      dailyRentalRate: 2,
+      genre: { name: '12345' },
+      numberInStock: 10
+    })
+    await movie.save()
 
     rental = new Rental({
       customer: {
@@ -41,6 +53,7 @@ describe('/api/returns', () => {
 
   afterEach(async () => {
     await Rental.remove({})
+    await Movie.remove({})
     await server.close()
   })
 
@@ -99,5 +112,40 @@ describe('/api/returns', () => {
     const diff = new Date() - rentalInDb.dateReturned
 
     expect(diff).toBeLessThan(10 * 1000)
+  })
+
+  it('should set the rental fee if input is valid', async () => {
+    rental.dateOut = moment()
+      .add(-7, 'days')
+      .toDate()
+    await rental.save()
+
+    const res = await exec()
+
+    const rentalInDb = await Rental.findById(rental._id)
+    expect(rentalInDb.rentalFee).toBe(14)
+  })
+
+  it('should increase the movie stock if input is valid', async () => {
+    const res = await exec()
+
+    const movieInDb = await Movie.findById(movieId)
+    expect(movieInDb.numberInStock).toBe(movie.numberInStock + 1)
+  })
+
+  it('should the rental if input is valid', async () => {
+    const res = await exec()
+
+    const rentalInDb = await Rental.findById(rental._id)
+
+    expect(Object.keys(res.body)).toEqual(
+      expect.arrayContaining([
+        'dateOut',
+        'dateReturned',
+        'rentalFee',
+        'customer',
+        'movie'
+      ])
+    )
   })
 })
